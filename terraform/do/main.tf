7a81edecd37c7934418df8c148d8d6d8d0c08445
Federico Luzzi <fedeluzzi00@gmail.com>
2023-04-20 17:07:56 +0200

    feat: add project, vpc and firewall resources to do config

--- terraform/do/main.tf
+++ terraform/do/main.tf
@@ -6,11 +6,47 @@ terraform {
   }
 }
 
-resource "digitalocean_kubernetes_cluster" "k8s" {
+resource "digitalocean_vpc" "k8s" {
+  name     = "k8s"
+  region   = "nyc1"
+  ip_range = "10.10.10.0/24"
+}
+
+resource "digitalocean_firewall" "k8s" {
+  name = "k8s"
+
+  # enable ipfs swarm
+  inbound_rule {
+    source_kubernetes_ids = digitalocean_kubernetes_cluster.main
+    protocol              = "tcp"
+    port_range            = "4001"
+  }
+
+  outbound_rule {
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol              = "tcp"
+    port_range            = "4001"
+  }
+
+  inbound_rule {
+    source_kubernetes_ids = digitalocean_kubernetes_cluster.main
+    protocol              = "udp"
+    port_range            = "4002"
+  }
+
+  outbound_rule {
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol              = "udp"
+    port_range            = "4002"
+  }
+}
+
+resource "digitalocean_kubernetes_cluster" "main" {
   name         = "api"
   version      = "1.26.3-do.0"
   region       = "nyc1"
   auto_upgrade = true
+  vpc_uuid     = digitalocean_vpc.k8s.id
 
   node_pool {
     name       = "worker-pool"
@@ -26,9 +62,13 @@ resource "digitalocean_domain" "main" {
   ip_address = data.kubernetes_ingress.main.status[0].load_balancer[0].ingress[0].ip
 }
 
-resource "digitalocean_certificate" "k8s-balancer" {
-  name    = "k8s-balancer"
+resource "digitalocean_certificate" "k8s-ingress" {
+  name    = "k8s-ingress"
   type    = "lets_encrypt"
   domains = [digitalocean_domain.main.name]
 }
 
+resource "digitalocean_project" "main" {
+  name      = "api"
+  resources = [digitalocean_kubernetes_cluster.main.urn, digitalocean_domain.main.urn]
+}
