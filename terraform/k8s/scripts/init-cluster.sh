3dc1c7fd13ca88e68ab46849bd9e523be9f0309c
Federico Luzzi <fedeluzzi00@gmail.com>
2023-05-31 10:32:53 +0100

    feat: add pinata and web3 explicit peers

--- terraform/k8s/scripts/init-cluster.sh
+++ terraform/k8s/scripts/init-cluster.sh
@@ -8,8 +8,8 @@ fi
 
 if [ "$(cat /proc/sys/kernel/hostname)" == "ipfs-node-0" ]; then
     CLUSTER_ID=${BOOTSTRAP_PEER_ID} \
-    CLUSTER_PRIVATEKEY=${BOOTSTRAP_PEER_PRIVATE_KEY} \
-    exec ipfs-cluster-service daemon --upgrade --leave
+        CLUSTER_PRIVATEKEY=${BOOTSTRAP_PEER_PRIVATE_KEY} \
+        exec ipfs-cluster-service daemon --upgrade --leave
 else
     exec ipfs-cluster-service daemon --upgrade --bootstrap /dns4/ipfs-node.api.svc.cluster.local/tcp/9096/ipfs/${BOOTSTRAP_PEER_ID} --leave
 fi
